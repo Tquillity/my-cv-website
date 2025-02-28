936b64dc49b5d18ae0cd423a3aadda2555f1207a
Tquillity <mikael.sundh@gmail.com>
2025-02-28 10:47:24 +0100

    removal x link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { useTranslation } from "../utils/i18n";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Footer: React.FC = () => {
   const { t } = useTranslation();
@@ -13,10 +13,7 @@ const Footer: React.FC = () => {
       <div className="footer-content">
         <p className="copyright">&copy; {currentYear} Mikael Sundh. {t('all_rights_reserved')}</p>
         <div className="social-links">
-          <a href="https://x.com/DigiSlinger" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
-          <a href="https://linkedin.com/in/mikael-sundh-9121743a/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+          <a href="https://www.linkedin.com/in/mikael-sundh/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
           <a href="https://github.com/Tquillity" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
